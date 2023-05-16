dc2a961636b889f71413582a9af483dc1b4d9843
Angelo <70825802+AngeloDiMortimer@users.noreply.github.com>
2023-05-16 17:39:51 -0400

    Ship factory function completed and passes test

--- src/app/modules/factories/ship.js
+++ src/app/modules/factories/ship.js
@@ -1,6 +1,7 @@
 import { SHIP_LENGTHS } from "../helpers/helpers";
 
 const Ship = (type) => {
+    //properties
     const id = type; //input sent from function call. IE: "destroyer"
     const length = SHIP_LENGTHS[type]; //IE: "Battleship: 4"
     let direction = "horizontal";
@@ -12,7 +13,15 @@ const Ship = (type) => {
         : (direction = "horizontal");
     };
 
-    return {id, length, getDirection, changeDirection};
+    //hits (i)
+    const hits = Array(length).fill(null); //creates new hit array depending of the length of the ship
+    const hit =  (i) => (hits[i] = "hit"); //changes ship's hits array index from null to hit
+    const getHits = () => hits; //displays state of hits array,
+
+    //isSunk
+    const isSunk = () => hits.every((h) => h === "hit"); //checks if ship is sunk, if all elements are "hit" return true
+
+    return {id, length, hit, getHits, isSunk, getDirection, changeDirection};
 };
 
 export default Ship;
--- src/app/modules/tests/ship.test.js
+++ src/app/modules/tests/ship.test.js
@@ -22,4 +22,35 @@ describe('Ship Factory', () => {
             expect(ship.getDirection()).toBe('vertical');
         });
     });
+
+    describe('Hit function', () => {
+        const ship = Ship('submarine');
+       
+        test('no hits', () => {
+            expect(ship.getHits()).toEqual([null, null, null]);
+        });
+       
+        test('one hit', () => {
+            ship.hit(2);
+            expect(ship.getHits()).toEqual([null, null, 'hit']);
+        });
+    });
+    
+    describe('isSunk function', () => {
+        const ship = Ship('destroyer');
+        test('not sunk', () => {
+            expect(ship.isSunk()).toBe(false);
+        });
+
+        test('hit but not sunk yet', () => {
+            ship.hit(0);
+            expect(ship.isSunk()).toBe(false);
+        });
+
+        test('ship is sunk', () => {
+            ship.hit(1);
+            expect(ship.isSunk()).toBe(true);
+        });
+    });
 });
+
