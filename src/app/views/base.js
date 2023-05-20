5a297d0f929bcd03d30da0dba91b10d5339ddd76
AngeloDiMortimer <angelodimortimer99@gmail.com>
2023-05-20 19:02:53 -0400

    created basic HTML and elements file

---
+++ src/app/views/base.js
@@ -0,0 +1,14 @@
+export const elements = {
+    p1Gameboard: document.querySelector('.p1-gameboard'),
+    p2Gameboard: document.querySelector('.p2-gameboard'),
+    p1Grid: document.querySelector('.p1-grid'),
+    p2Grid: document.querySelector('.p2-grid'),
+    fleetContainer: document.querySelector('.fleet-container'),
+    autoPlaceBtn: document.querySelector('.fleet-autoplace'),
+    fleetInfo: document.querySelector('.fleet-info'),
+    fleetDraggable: document.querySelector('.fleet-draggable'),
+    startBtn: document.querySelector('.start'),
+    infoContainer: document.querySelector('.info-container'),
+    infoText: document.querySelector('.info.text'),
+    playAgainBtn: document.querySelector('.play-again'),
+};
